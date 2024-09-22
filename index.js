e0427b0c823e4bac193fefc1c80c0df748cefb47
Andrii Boiko <andrii.m.boiko@gmail.com>
2024-09-22 15:30:58 +0200

    asda

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!1!!!asd');
+  res.send('Hello World!1!!!aasdsd');
   //res.status(404).send('Hello World!');
 });
 
